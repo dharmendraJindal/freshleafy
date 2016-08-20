23c68de4eced675997f8ab4511da7997a52d80c2
Dharmendra <djindaliitkgp@gmail.com>
2016-08-21 02:20:20 +0530

    server v0.12

--- freshleafy/urls.py
+++ freshleafy/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from freshleafy import view
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', view.index, name='index'),
 ]
