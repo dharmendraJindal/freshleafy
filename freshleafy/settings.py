1de9ec35dd6d4c6d64382bde867fd1903f56c0bf
Dharmendra <djindaliitkgp@gmail.com>
2016-08-21 01:55:56 +0530

    server v0.6

--- freshleafy/settings.py
+++ freshleafy/settings.py
@@ -118,5 +118,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 
-##   edit   ####
+##  ######################### edit   ####
 #STATIC_URL = '/static/'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
