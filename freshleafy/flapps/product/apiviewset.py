fab3c4b4f24f42e1bf8029ae8135d608758d4f10
Dharmendra <djindaliitkgp@gmail.com>
2016-08-21 14:11:52 +0530

    DJ: allow all user read only permission

--- freshleafy/flapps/product/apiviewset.py
+++ freshleafy/flapps/product/apiviewset.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers, viewsets
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 # Serializers define the API representation.
 from freshleafy.flapps.product.models import ProductCategory
@@ -12,5 +13,6 @@ class ProductCategorySerializer(serializers.HyperlinkedModelSerializer):
 
 # ViewSets define the view behavior.
 class ProductCategoryViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = ProductCategory.objects.all()
     serializer_class = ProductCategorySerializer
