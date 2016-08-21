da481b0d7d333d05daf9da98ec854b2a57c5ced8
Dharmendra <djindaliitkgp@gmail.com>
2016-08-21 13:18:50 +0530

    DJ: addinmg product category model

---
+++ freshleafy/flapps/product/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from freshleafy.flapps.product.models import ProductCategory
+
+
+admin.site.register(ProductCategory)
--- freshleafy/settings.py
+++ freshleafy/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #flapps
+    'freshleafy.flapps.product'
 ]
 
 MIDDLEWARE = [
