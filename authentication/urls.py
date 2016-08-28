624fd4f54e1663db78f35bce296b735499bdf321
dharmendraJindal <djindaliitkgp@gmail.com>
2016-08-28 11:55:58 +0530

    DJ: test

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,7 +6,7 @@ from authentication.views import AccountViewSet, LoginView, LogoutView
 router = routers.DefaultRouter()
 router.register(r'register', AccountViewSet)
 
-#test done
+#test done git
 
 urlpatterns = [
     url(r'^login/$', LoginView.as_view(), name='login'),
