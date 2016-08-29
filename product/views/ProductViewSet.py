44b400f4b0c97fd1f356414b5eeb59e58cf3996d
Dharmendra <djindaliitkgp@gmail.com>
2016-08-30 01:00:34 +0530

    DJ: added check user exist api

--- product/views/ProductViewSet.py
+++ product/views/ProductViewSet.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
 from product.models import Product
+from product.models import ProductCategory
 from product.serialisers.ProductSerialiser import ProductSerialiser
 
 
@@ -12,5 +13,13 @@ class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerialiser
 
     def list(self, request, *args, **kwargs):
+        product_category_name = request.GET.get('product_category_name', None)
+        if product_category_name:
+            try:
+                product_category = ProductCategory.objects.get(name=product_category_name)
+                self.queryset = Product.objects.filter(product_category=product_category)
+            except ProductCategory.DoesNotExist:
+                print "Product Category Not Found, Sending All Products"
+                pass
         serializer = self.serializer_class(self.queryset, many=True)
         return Response(serializer.data)
