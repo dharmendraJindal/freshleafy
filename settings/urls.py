3c11ca8ea55920a47e20adaa05dfedbc6cfaec02
Dharmendra <djindaliitkgp@gmail.com>
2016-09-03 13:18:52 +0530

    DJ: worked on home page

--- settings/urls.py
+++ settings/urls.py
@@ -5,14 +5,13 @@ from base.views.IndexView import IndexView
 from base.views.HomeView import HomeView
 
 urlpatterns = [
+    # url(r'^$', HomeView.as_view(), name='home'),
     url(r'^$', IndexView.as_view(), name='base'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/v1/auth/', include('authentication.urls')),
     url(r'^api/v1/product/', include('product.urls')),
     url(r'^api/v1/files/', include('image.urls')),
-    url(r'^home/', HomeView.as_view(), name='home'),
 ]
-
 
 #handling page not found
 
