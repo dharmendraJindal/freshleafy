3638ddc6746405c45d1e20fc604025ea7fb53dc9
Dharmendra <djindaliitkgp@gmail.com>
2016-09-03 12:26:47 +0530

    DJ: worked on home page

--- settings/urls.py
+++ settings/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 from base.views.IndexView import IndexView
+from base.views.HomeView import HomeView
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='base'),
@@ -9,6 +10,7 @@ urlpatterns = [
     url(r'^api/v1/auth/', include('authentication.urls')),
     url(r'^api/v1/product/', include('product.urls')),
     url(r'^api/v1/files/', include('image.urls')),
+    url(r'^home/', HomeView.as_view(), name='home'),
 ]
 
 
