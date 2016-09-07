bb6d92181812bd95a6ef24444164c9c5fe18bf94
Dharmendra <djindaliitkgp@gmail.com>
2016-09-07 21:24:41 +0530

    DJ: added ordersummaries

---
+++ product/views/UserOrderSummaryViewSet.py
@@ -0,0 +1,25 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+
+from product.models import Product
+from product.models import ProductCategory
+from product.serialisers.ProductSerialiser import ProductSerialiser
+
+
+class (viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Product.objects.all()
+    serializer_class = ProductSerialiser
+
+    def list(self, request, *args, **kwargs):
+        product_category_name = request.GET.get('product_category_name', None)
+        if product_category_name:
+            try:
+                product_category = ProductCategory.objects.get(name=product_category_name)
+                self.queryset = Product.objects.filter(product_category=product_category)
+            except ProductCategory.DoesNotExist:
+                print "Product Category Not Found, Sending All Products"
+                pass
+        serializer = self.serializer_class(self.queryset, many=True)
+        return Response(serializer.data)
---
+++ static/js/product/controllers/OrderSummaryController.js
@@ -0,0 +1,3 @@
+/**
+ * Created by dharmendra on 7/9/16.
+ */
