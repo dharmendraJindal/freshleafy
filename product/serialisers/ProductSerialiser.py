33ee9c9e06f3ac129dc8525ac12ff185c1e21de8
dharmendraJindal <djindaliitkgp@gmail.com>
2017-09-17 22:01:38 +0530

    DJ: added quaantity intervals

--- product/serialisers/ProductSerialiser.py
+++ product/serialisers/ProductSerialiser.py
@@ -9,13 +9,18 @@ class ProductSerialiser(serializers.ModelSerializer):
     product_category = ProductCategorySerializer(many=True)
     product_id = serializers.SerializerMethodField()
     hindi_name = serializers.SerializerMethodField()
+    quantity_intervals = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
-        fields = ('name','hindi_name' ,'image_path','product_category', 'grade', 'rate', 'unit', 'product_id')
+        fields = ('name','hindi_name' ,'image_path','product_category', 'grade', 'rate', 'unit', 'product_id',
+                  'quantity_intervals')
 
     def get_product_id(self, obj):
         return obj.id
 
     def get_hindi_name(self, obj):
         return obj.name
+
+    def get_quantity_intervals(self, obj):
+        return [1,5,10,25,50]
