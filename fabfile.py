4196c19d4e66d922f0f01860918429ae36379418
dharmendraJindal <djindaliitkgp@gmail.com>
2017-09-30 16:37:59 +0530

    DJ: working on deployment script

--- fabfile.py
+++ fabfile.py
@@ -9,23 +9,13 @@ def deploy():
     with cd('public/jindalfresh/freshleafy'):
         with_virtualenv()
         run("git pull")
-        run("pip install -r settings/requirements.pip")
+        # run("pip install -r settings/requirements.pip")
 
         print "Reloading project......................"
         run("sudo supervisorctl reread")
         run("sudo supervisorctl update")
 
         print "Done============******************************************"
-
-def shell():
-    local('python manage.py shell')
-
-
-def test_dev():
-    run('ls /home/webadmin')
-
-def project_dir():
-    cd('/home/webadmin/public/jindalfresh/freshleafy')
 
 def with_virtualenv():
     run('source /home/webadmin/.virtualenvs/jindalfresh/bin/activate')
