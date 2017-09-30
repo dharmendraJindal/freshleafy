d54ad40ab0620f43dade9ad1d91898f1cf3d1bf4
dharmendraJindal <djindaliitkgp@gmail.com>
2017-09-30 16:29:28 +0530

    DJ: working on deployment script

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
-from fabric.api import env, run
+from fabric.api import env, run, cd
 from fabric.colors import green, red, yellow
 
 env.user = "webadmin"
@@ -6,17 +6,16 @@ env.hosts = ["172.104.177.182"]
 
 def deploy():
     print "DEPLOYING============******************************************"
-    with_project_dir()
-    with_virtualenv()
-    run("git pull")
+    with cd('public/jindalfresh/freshleafy'):
+        with_virtualenv()
+        run("git pull")
+        run("pip install -r settings/requirements.pip")
 
-    run("pip install -r settings/requirements.pip")
+        print "Reloading project......................"
+        run("sudo supervisorctl reread")
+        run("sudo supervisorctl update")
 
-    print "Reloading project......................"
-    run("sudo supervisorctl reread")
-    run("sudo supervisorctl update")
-
-    print "Done============******************************************"
+        print "Done============******************************************"
 
 def shell():
     local('python manage.py shell')
@@ -25,8 +24,8 @@ def shell():
 def test_dev():
     run('ls /home/webadmin')
 
-def with_project_dir():
-    run('cd /home/webadmin/public/jindalfresh/freshleafy')
+def project_dir():
+    cd('/home/webadmin/public/jindalfresh/freshleafy')
 
 def with_virtualenv():
     run('source /home/webadmin/.virtualenvs/jindalfresh/bin/activate')
